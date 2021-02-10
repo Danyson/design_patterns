73788464683a206055103d77da324ec0ee3b2c3c
Danyson Jose Theres D <idanyson@gmail.com>
2021-02-10 20:16:32 +0530

    factory method example added

---
+++ creational_patterns/factory_method/factory_method.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class Engine(ABC):
+    @abstractmethod
+    def mileage(self):
+        pass
+
+class Car(Engine):
+    ''' assuming the car travels 15KM per Litre of Fuel'''
+    def __init__(self, fuelInLitre):
+        self.fuelInLitre = fuelInLitre
+    def mileage(self):
+        self.mileage = 15 * self.fuelInLitre
+        return self.mileage
+
+class Bike(Engine):
+    ''' assuming the bike travels 50KM per Litre of Fuel'''
+    def __init__(self, fuelInLitre):
+        self.fuelInLitre = fuelInLitre
+    def mileage(self):
+        self.mileage = 50 * self.fuelInLitre
+        return self.mileage
+
+
+class MileageCalculatorFactory():
+    def mileage_calculator(self, vehicleType, fuelInLitre):
+        if ( vehicleType == 'car' ):
+            car = Car(fuelInLitre)
+            return car.mileage()
+        elif ( vehicleType == 'bike' ):
+            bike = Bike(fuelInLitre)
+            return bike.mileage()
+
+mileage = MileageCalculatorFactory()
+print(f'{mileage.mileage_calculator("car", 5)}KMs')
+print(f'{mileage.mileage_calculator("bike", 5)}KMs')
